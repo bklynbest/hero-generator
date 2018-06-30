adc5f69a54f98a06920b87f18aaf551893f040d5
Jonathan Momperousse <bklynbest718@gmail.com>
2018-06-30 19:12:04 +0000

    installed faker gem, and added create a superhero action for index page, and updated views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,45 @@
 class HomeController < ApplicationController
+  before_action :common_action, :only => [:index, :team]
+  
   def index
   end
   
   def team
+    @team_number = params[:team_number].to_i
+  end
+  
+  def common_action 
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @corp_title = Faker::Name.title
+    @corp_power = Faker::Company.bs
+    @corp_power = @corp_power.titleize
+    
+    
+    @rand_num = rand(1..1000)
+    @set_number = rand(1..4)
+    @background = rand(1..2)
+    
+    #random backgrounds 
+    if @set_number == 1
+      @set = "set1"
+    elsif @set_number == 2
+      @set = "set2"
+    elsif @set_number == 3
+      @set = "set3"
+    else
+      @set = "set4"
+    end
+    
+    if @background == 1
+      @bg = "bg1"
+    else @background == 2
+      @bg = "bg2"
+    end
+    
+    @avatar = Faker::Avatar.image(@rand_num, "300x300", "bmp", @set, @bg)
+  
+    
   end
 end
+
